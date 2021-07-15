3b7fdf2bcc234e9a75cf27c924df56cdae392ac7
Sergey Karlinsky <4923920@gmail.com>
2021-07-15 10:19:31 +0300

    deploy commit

---
+++ middleware/checkRoleMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (role) {
+  return function (req, res, next) {
+    if (req.method === 'OPTIONS') {
+      next();
+    }
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
+        res.status(401).json({ message: 'Unauthorized' });
+      }
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      if (decoded.role !== role) {
+        res.status(403).json({ message: 'Not allowed' });
+      }
+      req.user = decoded;
+      console.log('test');
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  };
+};
